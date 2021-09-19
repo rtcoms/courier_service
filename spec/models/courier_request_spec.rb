e10505b9c7343a532e88f98a5f526a63852b4d15
Rohit Trivedi <86.rohit@gmail.com>
2021-09-19 16:34:56 +0530

    validation for db colums on courier_request

--- spec/models/courier_request_spec.rb
+++ spec/models/courier_request_spec.rb
@@ -2,6 +2,25 @@ require 'rails_helper'
 
 RSpec.describe CourierRequest, type: :model do
   describe 'db_columns' do
-    it { should have_db_column(:weight) }
+    it { should have_db_column(:weight).of_type(:integer)  }
+    it { should have_db_column(:cost).of_type(:float)  }
+    it { should have_db_column(:payment_mode).of_type(:integer)  }
+    it { should have_db_column(:status).of_type(:integer)  }
+
+    it { should have_db_column(:sender_fullname).of_type(:string)  }
+    it { should have_db_column(:sender_address).of_type(:text)  }
+    it { should have_db_column(:sender_phone).of_type(:string)  }
+    it { should have_db_column(:sender_pincode).of_type(:string)  }
+
+    it { should have_db_column(:receiver_fullname).of_type(:string)  }
+    it { should have_db_column(:receiver_address).of_type(:text)  }
+    it { should have_db_column(:receiver_phone).of_type(:string)  }
+    it { should have_db_column(:receiver_pincode).of_type(:string)  }
+
+    it { should have_db_column(:tracking_number).of_type(:string)  }
+
+    it { should have_db_column(:created_at).of_type(:datetime)  }
+    it { should have_db_column(:updated_at).of_type(:datetime)  }
+
   end
 end
